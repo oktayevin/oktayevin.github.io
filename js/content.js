db8b68f5f144aa886e4027e53a19f2c83e7a1ee5
Oktay Evin <63397576+oktayevin@users.noreply.github.com>
2025-11-04 20:50:19 +0100

    Aldım Sazı Elime

---
+++ js/content.js
@@ -0,0 +1,388 @@
+globalThis.cv = Object.assign(globalThis.cv || {}, {
+    "general": {
+      "profilePhoto": "https://firebasestorage.googleapis.com/v0/b/maitake-project.appspot.com/o/profilePhotos%2FmeThUjEtZvhYkGD0OhVklVd1Xyq1%2Fdafa3e46-0e6d-4bac-8a7e-a6ec65bee30b.jpg?alt=media&token=e9794dc1-e395-4f50-b8b6-eb654959838f",
+      "username": "oktayevin",
+      "displayName": "Oktay Evin",
+      "profession": "Student & iOS Developer",
+      "location": "İstanbul, Turkey",
+      "pronouns": null,
+      "byline": "Student & iOS Developer in İstanbul, Turkey",
+      "website": "https://oktayevin.com",
+      "websiteURL": "https://oktayevin.com",
+      "about": "I'm Oktay Evin, a visionary & interactive developer. Thanks to the technology teams and entrepreneurship programs I've been in since high school, I gained experience in coding, entrepreneurship, teamwork, strategic partnerships and company relations. Currently, I'm going to the university and working as a data analyst.",
+      "status": {
+        "text": null,
+        "emoji": null,
+        "timestamp": null
+      }
+    },
+    "projects": [],
+    "sideProjects": [],
+    "exhibitions": [],
+    "talks": [],
+    "writing": [],
+    "awards": [
+      {
+        "id": "7W6R4J4Tc485syEPrTIS",
+        "year": "2020",
+        "heading": "Highest Rookie Seed from First Robotic Competition (FRC)",
+        "url": "https://www.thebluealliance.com/team/8166/2020",
+        "collaborators": [],
+        "description": null,
+        "attachments": [],
+        "type": "award",
+        "title": "Highest Rookie Seed",
+        "presenter": "First Robotic Competition (FRC)"
+      },
+      {
+        "id": "QLPagdu5RBXNMTrwPibh",
+        "year": "2020",
+        "heading": "Rookie All Star from First Robotic Competition (FRC)",
+        "url": "https://www.thebluealliance.com/team/8166/2020",
+        "collaborators": [],
+        "description": null,
+        "attachments": [],
+        "type": "award",
+        "title": "Rookie All Star",
+        "presenter": "First Robotic Competition (FRC)"
+      },
+      {
+        "id": "8v287pocM31hGitNfM8c",
+        "year": "2019",
+        "heading": "Fintech Innovation Camp Winner from Arçelik, Citi & Junior Achievement",
+        "url": "https://www.gencbasari.org/duyuru",
+        "collaborators": [],
+        "description": null,
+        "attachments": [],
+        "type": "award",
+        "title": "Fintech Innovation Camp Winner",
+        "presenter": "Arçelik, Citi & Junior Achievement"
+      }
+    ],
+    "features": [],
+    "workExperience": [
+      {
+        "id": "nyd5QGTUAFtIIBjl8tR6",
+        "year": "2023 — Now",
+        "heading": "Chief Technology Officer at GoldenHas",
+        "url": "https://goldenhas.com.tr",
+        "collaborators": [],
+        "description": "",
+        "attachments": [],
+        "type": "workExperience",
+        "title": "Chief Technology Officer",
+        "company": "GoldenHas",
+        "location": "Istanbul"
+      },
+      {
+        "id": "m9pIO5LGOfPkqTHuRX9j",
+        "year": "2023 — Now",
+        "heading": "Data Analyst Specializing in Big Data and Predictive Analytics at Lead Gate Media GmbH",
+        "url": "https://www.leadgatemedia.com",
+        "collaborators": [],
+        "description": "I am a Data Analyst at Lead Gate Media GmbH, where I interpret complex data to provide actionable insights for strategic decision-making. My role encompasses utilizing advanced statistical tools to examine and dissect data sets, identifying critical trends, and patterns that inform marketing, sales, and customer engagement strategies. I manage the integrity and precision of data, translating my findings into clear, actionable reports for company stakeholders. Additionally, I create compelling data visualizations and reports that elucidate business performance, constantly seeking ways to refine our processes and enhance overall business performance.",
+        "attachments": [
+          {
+            "type": "image",
+            "width": 1024,
+            "height": 640,
+            "url": "https://res.cloudinary.com/read-cv/image/upload/c_limit,h_2048,w_2048/v1/1/profileItems/meThUjEtZvhYkGD0OhVklVd1Xyq1/m9pIO5LGOfPkqTHuRX9j/72bb03ee-3101-4be9-98a3-cd7c627e0d36.jpg?_a=DATAdtfiZAA0"
+          }
+        ],
+        "type": "workExperience",
+        "title": "Data Analyst Specializing in Big Data and Predictive Analytics",
+        "company": "Lead Gate Media GmbH",
+        "location": "Germany"
+      },
+      {
+        "id": "IZRaOxrqIUf2vcu8RQOy",
+        "year": "2019 — 2020",
+        "heading": "Sponsorship Manager at Intechne Robotics",
+        "url": null,
+        "collaborators": [],
+        "description": "Worked to establish strong communications with companies to cover production and organization costs.",
+        "attachments": [],
+        "type": "workExperience",
+        "title": "Sponsorship Manager",
+        "company": "Intechne Robotics",
+        "location": null
+      },
+      {
+        "id": "XMlBuWFv7bmdxugpxEum",
+        "year": "2018 — 2020",
+        "heading": "Public Relations Responsible at Intechne Robotics",
+        "url": null,
+        "collaborators": [],
+        "description": "Management of all aspects necessary for the correct promotion of the company to the public, from social media management to company promotions.",
+        "attachments": [],
+        "type": "workExperience",
+        "title": "Public Relations Responsible",
+        "company": "Intechne Robotics",
+        "location": null
+      }
+    ],
+    "volunteering": [],
+    "education": [
+      {
+        "id": "2iAGcxqm3Ewn2p5yZFfS",
+        "year": "2022 — Now",
+        "heading": "Industrial Design at Kadir Has University",
+        "url": "https://id.khas.edu.tr",
+        "collaborators": [],
+        "description": null,
+        "attachments": [],
+        "type": "education",
+        "degree": "Industrial Design",
+        "school": "Kadir Has University",
+        "location": null
+      },
+      {
+        "id": "Akn0YmhlN4UwhFBYqB1S",
+        "year": "2020 — 2022",
+        "heading": "Kartal Uğur Anatolian High School",
+        "url": "https://www.ugurokullari.k12.tr/okullarimiz/istanbul-kartal-ugur-anadolu-lisesi",
+        "collaborators": [],
+        "description": null,
+        "attachments": [],
+        "type": "education",
+        "degree": null,
+        "school": "Kartal Uğur Anatolian High School",
+        "location": null
+      },
+      {
+        "id": "UzgpWBLgwhO5mSjD93Iz",
+        "year": "2018 — 2020",
+        "heading": "Kırımlı Fazilet Olcay Anatolian High School",
+        "url": "https://kfoal.meb.k12.tr",
+        "collaborators": [],
+        "description": null,
+        "attachments": [],
+        "type": "education",
+        "degree": null,
+        "school": "Kırımlı Fazilet Olcay Anatolian High School",
+        "location": null
+      }
+    ],
+    "certifications": [],
+    "contact": [
+      {
+        "id": "b1DuhTv1lj6920NQELvh",
+        "platform": "Website",
+        "handle": "oktayevin.com",
+        "url": "https://oktayevin.com"
+      },
+      {
+        "id": "YjhpNR4sKbD1OfjdLCpM",
+        "platform": "Instagram",
+        "handle": "oktayevin",
+        "url": "https://instagram.com/oktayevin"
+      },
+      {
+        "id": "MpfH4bFtG7Wzzle3FLmy",
+        "platform": "Twitter",
+        "handle": "Oktay_Evin",
+        "url": "https://twitter.com/Oktay_Evin"
+      },
+      {
+        "id": "sXh6iHDq4s5zJTqk558C",
+        "platform": "LinkedIn",
+        "handle": "oktayevin",
+        "url": "https://linkedin.com/in/oktayevin"
+      },
+      {
+        "id": "cxfaiyDAeooYm5lJP1KF",
+        "platform": "GitHub",
+        "handle": "oktayevin",
+        "url": "https://github.com/oktayevin"
+      }
+    ],
+    "writingTab": {
+      "pages": []
+    },
+    "allCollections": [
+      {
+        "name": "Awards",
+        "items": [
+          {
+            "id": "7W6R4J4Tc485syEPrTIS",
+            "year": "2020",
+            "heading": "Highest Rookie Seed from First Robotic Competition (FRC)",
+            "url": "https://www.thebluealliance.com/team/8166/2020",
+            "collaborators": [],
+            "description": null,
+            "attachments": [],
+            "type": "award",
+            "title": "Highest Rookie Seed",
+            "presenter": "First Robotic Competition (FRC)"
+          },
+          {
+            "id": "QLPagdu5RBXNMTrwPibh",
+            "year": "2020",
+            "heading": "Rookie All Star from First Robotic Competition (FRC)",
+            "url": "https://www.thebluealliance.com/team/8166/2020",
+            "collaborators": [],
+            "description": null,
+            "attachments": [],
+            "type": "award",
+            "title": "Rookie All Star",
+            "presenter": "First Robotic Competition (FRC)"
+          },
+          {
+            "id": "8v287pocM31hGitNfM8c",
+            "year": "2019",
+            "heading": "Fintech Innovation Camp Winner from Arçelik, Citi & Junior Achievement",
+            "url": "https://www.gencbasari.org/duyuru",
+            "collaborators": [],
+            "description": null,
+            "attachments": [],
+            "type": "award",
+            "title": "Fintech Innovation Camp Winner",
+            "presenter": "Arçelik, Citi & Junior Achievement"
+          }
+        ]
+      },
+      {
+        "name": "Work Experience",
+        "items": [
+          {
+            "id": "nyd5QGTUAFtIIBjl8tR6",
+            "year": "2023 — Now",
+            "heading": "Chief Technology Officer at GoldenHas",
+            "url": "https://goldenhas.com.tr",
+            "collaborators": [],
+            "description": "",
+            "attachments": [],
+            "type": "workExperience",
+            "title": "Chief Technology Officer",
+            "company": "GoldenHas",
+            "location": "Istanbul"
+          },
+          {
+            "id": "m9pIO5LGOfPkqTHuRX9j",
+            "year": "2023 — Now",
+            "heading": "Data Analyst Specializing in Big Data and Predictive Analytics at Lead Gate Media GmbH",
+            "url": "https://www.leadgatemedia.com",
+            "collaborators": [],
+            "description": "I am a Data Analyst at Lead Gate Media GmbH, where I interpret complex data to provide actionable insights for strategic decision-making. My role encompasses utilizing advanced statistical tools to examine and dissect data sets, identifying critical trends, and patterns that inform marketing, sales, and customer engagement strategies. I manage the integrity and precision of data, translating my findings into clear, actionable reports for company stakeholders. Additionally, I create compelling data visualizations and reports that elucidate business performance, constantly seeking ways to refine our processes and enhance overall business performance.",
+            "attachments": [
+              {
+                "type": "image",
+                "width": 1024,
+                "height": 640,
+                "url": "https://res.cloudinary.com/read-cv/image/upload/c_limit,h_2048,w_2048/v1/1/profileItems/meThUjEtZvhYkGD0OhVklVd1Xyq1/m9pIO5LGOfPkqTHuRX9j/72bb03ee-3101-4be9-98a3-cd7c627e0d36.jpg?_a=DATAdtfiZAA0"
+              }
+            ],
+            "type": "workExperience",
+            "title": "Data Analyst Specializing in Big Data and Predictive Analytics",
+            "company": "Lead Gate Media GmbH",
+            "location": "Germany"
+          },
+          {
+            "id": "IZRaOxrqIUf2vcu8RQOy",
+            "year": "2019 — 2020",
+            "heading": "Sponsorship Manager at Intechne Robotics",
+            "url": null,
+            "collaborators": [],
+            "description": "Worked to establish strong communications with companies to cover production and organization costs.",
+            "attachments": [],
+            "type": "workExperience",
+            "title": "Sponsorship Manager",
+            "company": "Intechne Robotics",
+            "location": null
+          },
+          {
+            "id": "XMlBuWFv7bmdxugpxEum",
+            "year": "2018 — 2020",
+            "heading": "Public Relations Responsible at Intechne Robotics",
+            "url": null,
+            "collaborators": [],
+            "description": "Management of all aspects necessary for the correct promotion of the company to the public, from social media management to company promotions.",
+            "attachments": [],
+            "type": "workExperience",
+            "title": "Public Relations Responsible",
+            "company": "Intechne Robotics",
+            "location": null
+          }
+        ]
+      },
+      {
+        "name": "Education",
+        "items": [
+          {
+            "id": "2iAGcxqm3Ewn2p5yZFfS",
+            "year": "2022 — Now",
+            "heading": "Industrial Design at Kadir Has University",
+            "url": "https://id.khas.edu.tr",
+            "collaborators": [],
+            "description": null,
+            "attachments": [],
+            "type": "education",
+            "degree": "Industrial Design",
+            "school": "Kadir Has University",
+            "location": null
+          },
+          {
+            "id": "Akn0YmhlN4UwhFBYqB1S",
+            "year": "2020 — 2022",
+            "heading": "Kartal Uğur Anatolian High School",
+            "url": "https://www.ugurokullari.k12.tr/okullarimiz/istanbul-kartal-ugur-anadolu-lisesi",
+            "collaborators": [],
+            "description": null,
+            "attachments": [],
+            "type": "education",
+            "degree": null,
+            "school": "Kartal Uğur Anatolian High School",
+            "location": null
+          },
+          {
+            "id": "UzgpWBLgwhO5mSjD93Iz",
+            "year": "2018 — 2020",
+            "heading": "Kırımlı Fazilet Olcay Anatolian High School",
+            "url": "https://kfoal.meb.k12.tr",
+            "collaborators": [],
+            "description": null,
+            "attachments": [],
+            "type": "education",
+            "degree": null,
+            "school": "Kırımlı Fazilet Olcay Anatolian High School",
+            "location": null
+          }
+        ]
+      },
+      {
+        "name": "Contact",
+        "items": [
+          {
+            "id": "b1DuhTv1lj6920NQELvh",
+            "platform": "Website",
+            "handle": "oktayevin.com",
+            "url": "https://oktayevin.com"
+          },
+          {
+            "id": "YjhpNR4sKbD1OfjdLCpM",
+            "platform": "Instagram",
+            "handle": "oktayevin",
+            "url": "https://instagram.com/oktayevin"
+          },
+          {
+            "id": "MpfH4bFtG7Wzzle3FLmy",
+            "platform": "Twitter",
+            "handle": "Oktay_Evin",
+            "url": "https://twitter.com/Oktay_Evin"
+          },
+          {
+            "id": "sXh6iHDq4s5zJTqk558C",
+            "platform": "LinkedIn",
+            "handle": "oktayevin",
+            "url": "https://linkedin.com/in/oktayevin"
+          },
+          {
+            "id": "cxfaiyDAeooYm5lJP1KF",
+            "platform": "GitHub",
+            "handle": "oktayevin",
+            "url": "https://github.com/oktayevin"
+          }
+        ]
+      }
+    ]
+  });
